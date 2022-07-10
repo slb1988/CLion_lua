e7da3d51f84241552536b8777dfc8509df80ecf3
Luckey_Sun <sunlaibing88@gmail.com>
2022-07-10 18:16:34 +0800

    add ignore

--- lua-5.4/cmake-build-debug/cmake_install.cmake
+++ lua-5.4/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/sun/CLionProjects/lua-5.4
+# Install script for directory: /Users/sun/Documents/GitHub/CLion_lua/lua-5.4
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/sun/CLionProjects/lua-5.4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/sun/Documents/GitHub/CLion_lua/lua-5.4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
